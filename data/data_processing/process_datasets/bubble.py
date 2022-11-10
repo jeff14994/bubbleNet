dbaf115187973006be0310df00c524a0dad75032
Truxten Cook <45113873+TruxtenC@users.noreply.github.com>
2022-11-10 16:22:11 -0700

    Add attrs to bubble dataset

--- data/data_processing/process_datasets/bubble.py
+++ data/data_processing/process_datasets/bubble.py
@@ -1,3 +1,4 @@
+import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
 from pyspark.sql.types import StructType
 
@@ -7,6 +8,8 @@ attacks_to = StructType([])
 
 
 def get_bubbles(df: DataFrame, geo_ip_df: DataFrame) -> DataFrame:
+    df = df.withColumn("Category", F.col("Category").getItem(0))
+    df = df.withColumn("ProtocolType", F.col("Source.Proto").getItem(0).getItem(0))
     # assume for now that we only want the source country
     geo_enriched_data_src = get_source_data(
         df, geo_ip_df, include_long_lat=True, include_region=True
@@ -25,12 +28,20 @@ def get_bubbles(df: DataFrame, geo_ip_df: DataFrame) -> DataFrame:
         "TargetRegion",
     )
     geo_enriched_data_src = geo_enriched_data_src.select(
-        "ID", "SourceCountry", "SourceLongitude", "SourceLatitude", "SourceRegion"
+        "ID",
+        "SourceCountry",
+        "SourceLongitude",
+        "SourceLatitude",
+        "SourceRegion",
+        "Category",
+        "ProtocolType",
+        "ConnCount"
     )
     # let's join the two dataframes
     geo_enriched_data = geo_enriched_data_src.join(
         geo_enriched_data_tgt, on="ID", how="inner"
     )
+
     return geo_enriched_data.select(
         "ID",
         "EventTime",
@@ -42,4 +53,6 @@ def get_bubbles(df: DataFrame, geo_ip_df: DataFrame) -> DataFrame:
         "TargetLatitude",
         "TargetLongitude",
         "TargetRegion",
+        "Category",
+        "ConnCount",
     )
