a1b3977e8372d19adebf2e5d2dee71fff697aeec
axiao3 <72429720+axiao3@users.noreply.github.com>
2022-11-23 04:01:27 -0700

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -49,8 +49,18 @@ document.addEventListener('DOMContentLoaded', function () {
         heatmapData = values[0];
         showSpinner(false);
         bubble(data, svg, projection, globalData.date);
-        // TODO(Anrui Xiao): please see issue list(https://docs.google.com/document/d/1aUH-5f93TWAcMBsvGlnlNqm7SvuLBR-ap1dL5g7qKEI/edit)
+	    
+        /*
+        // with parameters: data, country, date, time, category, protocol type
+        //note that category and protocol type cannot be selected at the same time.
+        //examles: 
+        //time_alert(data,"whole world","Sun Mar 10 2019","17","Recon.Scanning",""): select heatmap and bullet 
+        //time_alert(data,"whole world","Sun Mar 10 2019","17","",""): select heatmap while global time changed
+        //time_alert(data,"whole world","Sun Mar 10 2019","","","tcp"): select bullet while global attribute changed
+        */
         time_alert(data,"whole world","Sun Mar 10 2019");//data seg, current selected country, date
+        //change to:   time_alert(data,"whole world","Sun Mar 10 2019","","",""); as default
+	    
         // default: set the amount of data to be displayed by countryNum
         //! Caution: If you want to increase the countryNum, remember
         //! to change the color scale in the function heatmap
